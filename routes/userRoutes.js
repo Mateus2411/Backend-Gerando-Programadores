e46dcb7386e66c8f2d8c2781ede637f026ed2ec9
Mateus2411 <mateushenriquedasilva2411@gmail.com>
2026-02-05 01:32:41 -0300

    DELETE/FEAT: Ajuste de nome de arquivo alem de salvar o token na nuvem junto com o midleware e nova rota para verificar se esta logado

---
+++ routes/userRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const {
+  register,
+  login,
+  usersDb,
+  tokenValidate,
+  coffee,
+} = require("../controllers/authControllers");
+const auth = require("../middlewares/auth");
+
+// Rotas públicas
+router.post("/register", register);
+router.post("/login", login);
+router.get("/validate", tokenValidate);
+router.get("/banco", usersDb);
+router.get("/coffee", coffee);
+
+router.get("/auth/me", auth, (req, res) => {
+  res.json({
+    teste: true,
+    msg: "Rota funcionando"
+  });
+});
+
+module.exports = router;
