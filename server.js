1b61d88dd1c534c05b62b2bacc87e06ada4b1d0d
Mateus2411 <mateushenriquedasilva2411@gmail.com>
2026-02-05 01:35:08 -0300

    FEAT: mlhora na utilizaçao do cors e desenvolvimento de utilização de salvamento do token na api

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
-const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-app.use(cors());
+
+// 🔴 CORS CORRETO PARA COOKIE
+// app.use(cors({
+//   origin: "https://gerando-programadores.vercel.app/",
+//   credentials: true,
+// }));
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
+// 🔴 habilita leitura de cookies
+app.use(cookieParser());
+
+app.use("/api", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
