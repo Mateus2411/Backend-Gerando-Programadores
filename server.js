6ae294aee5041be0311e8cb66555ec202fa90b2f
Mateus2411 <mateushenriquedasilva2411@gmail.com>
2026-02-22 22:49:35 -0300

    FIX: Pequeno ajuste

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use(cookieParser());
 
 app.use("/api", userRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta http://localhost:${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Servidor rodando na porta http://localhost:${PORT}`));
